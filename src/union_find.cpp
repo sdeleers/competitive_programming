ff3253f3038cdca468e3b57a866df271327fd9ef
Simon Deleersnijder <simon.deleersnyder@gmail.com>
2020-05-14 13:21:03 +0200

    Update union_find.cpp

--- src/union_find.cpp
+++ src/union_find.cpp
@@ -8,6 +8,27 @@ struct UnionFind {
     return parent[x] < 0 ? x : parent[x] = findComponent(parent[x]);
   }
 
+  bool sameSet(int a, int b) {
+    return findComponent(a) == findComponent(b);
+  }
+  
+  vector<vector<int>> getConnectedComponents() {
+    unordered_map<int, vector<int>> components_map;
+    for (int i = 0; i < parent.size(); ++i) {
+      int component = findComponent(i);
+      if (components_map.find(component) == components_map.end()) {
+        components_map[component] = { i };
+      } else {
+        components_map[component].push_back(i);
+      }
+    }
+    vector<vector<int>> components;
+    for (auto p : components_map) {
+      components.push_back(p.second);
+    }
+    return components;
+  }
+  
   void join(int a, int b) {
     a = findComponent(a), b = findComponent(b);
     if (a == b) return;
