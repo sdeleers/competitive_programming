9b75f4d6f6aa4469a7d3211b59096bcc1946aa27
Simon Deleersnijder <simon.deleersnyder@gmail.com>
2021-01-16 10:26:32 +0100

    Update dijkstra.cpp

--- src/dijkstra.cpp
+++ src/dijkstra.cpp
@@ -1,47 +1,60 @@
-struct Edge {
-  int to;
-  long long w;
-};
+struct Dijkstra {
 
-struct Vertex {
-  int v; // Vertex index.
-  long long d; // Current shortest path estimate from source to vertex.
-  bool done; // Whether we've already computed this vertex's shortest path.
+  struct Edge {
+    int to;
+    int64_t w;
+  };
 
-  bool operator <(const Vertex& v) const {
-    return d < v.d;
-  }
+  struct Vertex {
+    int v; // Vertex index.
+    int64_t d; // Current shortest path estimate from source to vertex.
+    bool done; // Whether we've already computed this vertex's shortest path.
 
-  bool operator >(const Vertex& v) const {
-    return d > v.d;
-  }
-};
-
-vector<Vertex> dijkstra(const vector<vector<Edge>>& graph, int start, int N) {
-  // Init.
-  vector<Vertex> vertices(N);
-  for (int i = 0; i < N; ++i) {
-    vertices[i] = { i, LONG_LONG_MAX, false };
-  }
-  vertices[start].d = 0;
-
-  priority_queue<Vertex, vector<Vertex>, greater<Vertex>> pq;
-  pq.push({ start, 0, false });
-  while (!pq.empty()) {
-    Vertex front = pq.top(); pq.pop();
-    int u = front.v;
-    if (vertices[u].done) continue;
-    for (int i = 0; i < graph[u].size(); ++i) {
-      Edge e = graph[u][i];
-      int from = u;
-      int to = e.to;
-      long long w = e.w;
-      if (vertices[from].d + w < vertices[to].d) {
-        vertices[to].d = vertices[from].d + w;
-        pq.push({ to, vertices[to].d, false });
-      }
+    bool operator <(const Vertex& v) const {
+      return d > v.d;
     }
-    vertices[u].done = true;
+  };
+
+  vector<vector<Edge>> graph;
+
+  Dijkstra(int num_vertices) : graph(num_vertices) {}
+
+  void addEdge(int from, int to, int64_t w) {
+    graph[from].push_back({ to, w });
   }
-  return vertices;
-}
+
+  vector<int64_t> solve(int start) {
+    // Init.
+    vector<Vertex> vertices(graph.size());
+    for (int i = 0; i < graph.size(); ++i) {
+      vertices[i] = { i, numeric_limits<int64_t>::max(), false };
+    }
+    vertices[start].d = 0;
+
+    priority_queue<Vertex> pq;
+    pq.push({ start, 0, false });
+    while (!pq.empty()) {
+      Vertex front = pq.top(); pq.pop();
+      int u = front.v;
+      if (vertices[u].done) continue;
+      for (int i = 0; i < graph[u].size(); ++i) {
+        Edge e = graph[u][i];
+        int from = u;
+        int to = e.to;
+        int64_t w = e.w;
+        if (vertices[from].d + w < vertices[to].d) {
+          vertices[to].d = vertices[from].d + w;
+          pq.push({ to, vertices[to].d, false });
+        }
+      }
+      vertices[u].done = true;
+    }
+    vector<int64_t> dist;
+    dist.reserve(vertices.size());
+    for (const auto& vertex : vertices) {
+      dist.push_back(vertex.d);
+    }
+    return dist;
+  }
+
+};
